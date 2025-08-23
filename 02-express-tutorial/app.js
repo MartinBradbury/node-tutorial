472decd99757c1b9b1746151c44df040f270731f
Martin Bradbury <martinbradbury@btinternet.com>
2025-08-23 21:01:23 +0100

    Express basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,26 @@
-console.log('Express Tutorial')
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// setup static and middleware -> can dump all static files here and expess will deal with it
+// public is common name for the static files in express
+// You can also just add index.html to this too. 
+app.use(express.static('./public'))
+
+
+// app.get('/', (req,res)=>{
+//     res.sendFile(path.join(__dirname,'./navbar-app/index.html'))
+        // ADD TO STATIC ASSETS OR SSR
+
+// })
+
+app.all('*',(req,res)=>{
+    res.status(404).send('resource not found')
+})
+
+
+
+app.listen(5000, () => {
+    console.log('Starting server on port 5000...')
+})
--- 02-express-tutorial/final/03-express-basics.js
+++ 02-express-tutorial/final/03-express-basics.js
@@ -1,22 +1,27 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 
-app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
+
+app.get('/',(req, res)=>{
+    console.log('User hit the resource')
+    res.status(200).send('Home Page')
 })
 
 app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+    console.log('User hit the resource')
+    res.status(200).send('About Page')
 })
 
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
+    console.log('User hit the resource')
+    res.status(404).send('404 Error')
 })
 
+
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+    console.log('server is up on port 5000')
 })
+
 
 // app.get
 // app.post
